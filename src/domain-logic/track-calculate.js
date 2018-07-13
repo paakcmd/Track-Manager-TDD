308a98ec6b5eadffa6aab1c08ec0eb4edfc5cccc
paakcmd <paakcmd@users.noreply.github.com>
2018-07-13 22:52:11 +0700

    track-calculate almost done

--- src/domain-logic/track-calculate.js
+++ src/domain-logic/track-calculate.js
@@ -1,27 +1,21 @@
+export const trackCalculate = function(numbers, target, goal = []) {
+  var sum = 0;
+  for (var i = 0; i < goal.length; i++) {
+    sum += goal[i];
+  }
 
-export const trackCalculate = function(numbers, target=15, goal=[]) {
-    var sum;
-    if(goal.length>0) {
-      sum = goal.reduce(function(a, b) { return a + b; });
-    }
-    else {
-      sum = 0;
-    }
-    if(sum === target) {
-      console.log(goal);
-    }
-    if( sum >= target) {
-      return
-    }
-    console.log(sum)
+  if (sum === target) {
+    console.log(goal)
+  }
+  if (sum >= target) {
+    return
+  }
 
-    for(var i = 0; i < numbers.length; i++) {
-      var number = numbers[i]
-      var remaining = numbers.slice(i+1,numbers.length)
-
-      goal.push(number);
-      console.log(remaining)
-      console.log(goal);
-      trackCalculate(remaining, target, goal);
-    }
-}
+  for (var j = 0; j < numbers.length; j++) {
+    var number = numbers[j];
+    var remaining = numbers.slice(j+1, numbers.length+1);
+    var newGoal = goal.slice(0,goal.length);
+    newGoal.push(number)
+    trackCalculate(remaining, target, newGoal);
+  }
+};
--- src/reducers/track.js
+++ src/reducers/track.js
@@ -10,8 +10,9 @@ const initialState = {
 const trackReducer = (state = initialState, action) => {
   switch (action.type) {
     case CREATE_TRACK:
-      var a = trackCalculate([1,2,3,4,5,6,7,8,9,10],15)
-      console.log(a);
+      var a = trackCalculate([9,6,2,4,1],15)
+      console.log(a)
+
       readFile(action.file).then(result => timeInterpret(result))
       return state;
     default:
