72f1240b6c4e70dd6e19e7e28612ec14a9ca0923
paakcmd <paakcmd@users.noreply.github.com>
2018-07-13 15:37:58 +0700

    Initialize Reducer

---
+++ src/reducers/track.js
@@ -0,0 +1,16 @@
+import { CREATE_TRACK } from '../actions/constants';
+
+const initialState = {
+  tracks: {}
+};
+
+const trackReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CREATE_TRACK:
+      return state;
+    default:
+      return state;
+  }
+};
+
+export default trackReducer;
--- src/reducers/track.test.js
+++ src/reducers/track.test.js
@@ -1,2 +1,12 @@
-import { trackReducer } from './track';
+import trackReducer from './track';
 import { createTrack } from '../actions/track';
+
+describe('trackReducer', () => {
+  describe('when initializing', () => {
+    const initialState = { tracks: {} };
+
+    it('sets an initial state', () => {
+      expect(trackReducer(undefined, {})).toEqual(initialState);
+    });
+  });
+});
