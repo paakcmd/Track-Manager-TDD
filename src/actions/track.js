7a6f60c86be2a4733efd92262cdc61bb2057cef4
paakcmd <paakcmd@users.noreply.github.com>
2018-07-14 00:03:27 +0700

    revert

--- src/actions/track.js
+++ src/actions/track.js
@@ -1,11 +1,3 @@
 import { CREATE_TRACK } from './constants';
-import { readFile } from '../reducers/helper/read-file';
-import { timeInterpret } from '../domain-logic/time-interpret';
-import { trackCalculate } from '../domain-logic/track-calculate';
 
-export const createTrack = file => {
-  readFile(file).then(result =>
-    console.log(timeInterpret(result))
-  )
-
-}
+export const createTrack = (file) => ({ type: CREATE_TRACK, file })
--- src/reducers/track.js
+++ src/reducers/track.js
@@ -1,5 +1,7 @@
 import { CREATE_TRACK } from '../actions/constants';
-
+import { readFile } from './helper/read-file';
+import { timeInterpret } from '../domain-logic/time-interpret';
+import { trackCalculate } from '../domain-logic/track-calculate';
 
 const initialState = {
   tracks: {}
@@ -8,7 +10,9 @@ const initialState = {
 const trackReducer = (state = initialState, action) => {
   switch (action.type) {
     case CREATE_TRACK:
-
+      var a = trackCalculate([9,6,2,4,1],15)
+      console.log(a);
+      readFile(action.file).then(result => timeInterpret(result))
       return state;
     default:
       return state;
